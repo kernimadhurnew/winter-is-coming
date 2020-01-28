86639b2ef24ea0a3e69409470320e8bbc1c6c3c9
kernimadhurnew <60299565+kernimadhurnew@users.noreply.github.com>
2020-01-29 00:40:19 +0530

    Added exception Handling for /battle/{id}

--- winter-is-coming/winter-is-coming/src/main/java/com/got/api/Controller/GOTController.java
+++ winter-is-coming/winter-is-coming/src/main/java/com/got/api/Controller/GOTController.java
@@ -1,13 +1,12 @@
 package com.got.api.Controller;
 
-
-
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import com.got.api.Exceptions.GOTExceptions;
 import com.got.api.model.ListModel;
 import com.got.api.repository.BattleRepository;
 
@@ -19,19 +18,17 @@ public class GOTController {
 	BattleRepository battleRepositry;
 	
 	@GetMapping("/list")
-	public List<ListModel> getAllRegion(){
-		return battleRepositry.findList();
+	public List<Object[]> getAllRegion(){
+		return battleRepositry.findEmp_name();
 	}
 	
 	@GetMapping("/count")
 	public int getCount() {
-		return battleRepositry.count();
+		return battleRepositry.counts();
 	}
 	
 	@GetMapping("/battle/{id}")
-	public Optional<ListModel> getBattle(@PathVariable String id) {
-															//need to handle the exception here for - of id doesn't exists in the records
-		return battleRepositry.getBattleById(id);
-
-	}
+	public ListModel getBattle(@PathVariable String id) {
+		 return Optional.ofNullable(battleRepositry.findByBattle(id)).orElseThrow(() -> new GOTExceptions("Battle Number Not Found"));
+}
 }
